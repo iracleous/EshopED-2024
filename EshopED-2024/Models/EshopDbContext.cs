ce8b1a91c06331018e9e80fc3b848231f40bad67
Dimitris Iracleous <diracleous@athtech.gr>
2024-11-13 10:15:14 +0200

    Add project files.

---
+++ EshopED-2024/Models/EshopDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2.Models;
+
+public class EshopDbContext:DbContext
+{
+    public DbSet<Product> Products { get; set; }  
+
+    public EshopDbContext(DbContextOptions<EshopDbContext> options): base(options)
+    {
+    }
+}
